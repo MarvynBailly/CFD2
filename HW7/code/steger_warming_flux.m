135e8c0603547c24bfb518d4b6861b0d605d54b1
Marvyn B <marvyn@marvyn.com>
2025-04-10 14:40:52 -0400

    cleaned up code

--- HW7/code/steger_warming_flux.m
+++ HW7/code/steger_warming_flux.m
@@ -1,49 +1,37 @@
-function [Fhp, Fhm] = steger_warming_flux(Qh, A, gamma)
+function [Fhp, Fhm] = steger_warming_flux(Q, A, gamma)
 % Steger-Warming flux vector splitting
-% assumes 1D Euler equations in conservative form
-%   Qh = [rho*A, rho*u*A, rho*e*A] 
-% so have to divide by A and play around to get back the primitive variables
-%   Q = [rho, u, p]
 % return fluxes in conservative form by multiplying by A
 
-    N = size(Qh, 2);
+    N = size(Q, 2);
     Fp = zeros(3, N);
     Fm = zeros(3, N);
 
-    for j = 1:N
-        % convert back to primitive variables
-        rho = Qh(1, j) / A(j);
-        rhou = Qh(2, j) / A(j);        
-        e = Qh(3, j) / A(j);
-        
-        u = rhou / rho;
-        u2 = u^2;
-        p = (gamma - 1) * (e - 0.5 * rho * u2);
+    rho = Q(1, :);
+    u = Q(2, :);
+    p = Q(3, :);
 
-        % compute speed of sound and enthalpy
-        c = sqrt(gamma * p / rho);
-        H = (e + p) / rho;
+    e = (p / (gamma - 1)) + 0.5 * rho .* u.^2;
+    u2 = u.^2;
+    c = sqrt(gamma * p ./ rho);
+    H = (e + p) ./ rho;
 
-        % compute eigenvalues
-        lambda1 = u;
-        lambda2 = u + c;
-        lambda3 = u - c;
+    lambda1 = u;
+    lambda2 = u + c;
+    lambda3 = u - c;
 
-        % split the evals
-        lambda1p = 0.5 * (lambda1 + abs(lambda1)); lambda1m = 0.5 * (lambda1 - abs(lambda1));
-        lambda2p = 0.5 * (lambda2 + abs(lambda2)); lambda2m = 0.5 * (lambda2 - abs(lambda2));
-        lambda3p = 0.5 * (lambda3 + abs(lambda3)); lambda3m = 0.5 * (lambda3 - abs(lambda3));
-        
-        % compute fluxes directly from provided formula
-        coef = 1 / (2 * gamma);
-        Fp(1, j) = coef * (2*(gamma - 1) * lambda1p + lambda2p + lambda3p);
-        Fp(2, j) = coef * (2*(gamma - 1) *u * lambda1p + lambda2 * lambda2p + lambda3 * lambda3p);
-        Fp(3, j) = coef * (2*(gamma - 1) * u2 * lambda1p + (H + u*c) * lambda2p + (H - u*c) * lambda3p);
-        
-        Fm(1, j) = coef * (2*(gamma - 1) * lambda1p + lambda2p + lambda3p);
-        Fm(2, j) = coef * (2*(gamma - 1) *u * lambda1p + lambda2 * lambda2p + lambda3 * lambda3p);
-        Fm(3, j) = coef * (2*(gamma - 1) * u2 * lambda1p + (H + u*c) * lambda2p + (H - u*c) * lambda3p);
-    end
+    lambda1p = 0.5 * (lambda1 + abs(lambda1)); lambda1m = 0.5 * (lambda1 - abs(lambda1));
+    lambda2p = 0.5 * (lambda2 + abs(lambda2)); lambda2m = 0.5 * (lambda2 - abs(lambda2));
+    lambda3p = 0.5 * (lambda3 + abs(lambda3)); lambda3m = 0.5 * (lambda3 - abs(lambda3));
+
+    % compute fluxes directly from provided formula
+    coef = 1 / (2 * gamma);
+    Fp(1, :) = coef * (2*(gamma - 1) * lambda1p + lambda2p + lambda3p);
+    Fp(2, :) = coef * (2*(gamma - 1) *u .* lambda1p + lambda2 .* lambda2p + lambda3 .* lambda3p);
+    Fp(3, :) = coef * (2*(gamma - 1) * u2 .* lambda1p + (H + u.*c) .* lambda2p + (H - u.*c) .* lambda3p);
+    
+    Fm(1, :) = coef * (2*(gamma - 1) * lambda1p + lambda2p + lambda3p);
+    Fm(2, :) = coef * (2*(gamma - 1) * u .* lambda1p + lambda2 .* lambda2p + lambda3 .* lambda3p);
+    Fm(3, :) = coef * (2*(gamma - 1) * u2 .* lambda1p + (H + u.*c) .* lambda2p + (H - u.*c) .* lambda3p);
 
     % convert back to conservative form
     Fhp = Fp .* A;
--- HW7/code/temp.m
+++ HW7/code/temp.m
@@ -34,69 +34,34 @@ for jmax = jmaxes
     [rho_sp,u_sp,p_sp,e_sp,amach_sp] = spacemarch(gamma,fsmach,p0,rho0,xsh,x,area,march_type);
   end
 
-  % plot initial conditions
-  figure(1)
-  subplot(2,1,1)
-  plot(x, rho_sp, 'r-', 'LineWidth', 2)
-  title('Density')
-  xlabel('x')
-  ylabel('Density')
-  grid on
-  subplot(2,1,2)
-  plot(x, p_sp, 'r-', 'LineWidth', 2)
-  title('Pressure')
-  xlabel('x')
-  ylabel('Pressure')
-  grid on
-
   % form conservative variables
   Qh = zeros(3, jmax);
-  Fh = zeros(3, jmax);
+  Q = zeros(3, jmax);
 
-  for j = 1:jmax
-    Fh(1, j) = rho_sp(j) * u_sp(j) * area(j);
-    Fh(2, j) = (rho_sp(j) * u_sp(j)^2 + p_sp(j)) * area(j);
-    Fh(3, j) = (e_sp(j) + p_sp(j)) * u_sp(j) * area(j);
-
-    Qh(1, j) = rho_sp(j) * area(j);
-    Qh(2, j) = rho_sp(j) * u_sp(j) * area(j);
-    Qh(3, j) = e_sp(j)* area(j);
-  end
-
+  Qh = convert_to_conservative(Qh, rho_sp, u_sp, e_sp, area);
 
   %%%%%% Explicit Time Marching %%%%%%
   % use Steger-Warming method to compute fluxes
-  res = zeros(3, jmax);
-  for t = 1:20
-    % recover primitive variables
-    rho = Qh(1, :) ./ area;
-    rhou = Qh(2, :) ./ area;        
-    e = Qh(3, :) ./ area;
-    
-    u = rhou ./ rho;
-    u2 = u.^2;
-    p = (gamma - 1) .* (e - 0.5 * rho .* u2);
+  for t = 1:10
+    % compute primitive variables
+    Q = convert_to_primitive(Q, Qh, gamma, area);
 
     % compute speed of sound and enthalpy
-    c = sqrt(gamma .* p ./ rho);
+    c = sqrt(gamma .* Q(3,:) ./ Q(1,:));
 
     % compute local timesteps
-    dt_local = cfl * dx ./ (abs(u) + abs(c));
-    dt = min(dt_local, [], "all");
-    
+    dt = compute_timestep(cfl, dx, Q(2,:), c);    
 
     % compute fluxes
-    [Fhp, Fhm] = steger_warming_flux(Qh, area, gamma);
+    [Fhp, Fhm] = steger_warming_flux(Q, area, gamma);
 
     % FIX BOUNDARY CONDITIONS
     % compute residuals 
-    for j = 2:jmax-1
-      res(:, j) = -dt / dx * (Fhp(:, j) - Fhp(:, j-1) + Fhm(:, j+1) - Fhm(:, j));
-      res(2, j) = res(2,j) + dt / dx * (p(j) * (0.5*(area(j+1)-area(j)) - 0.5*(area(j)-area(j-1))));
-    end
+    res = compute_residual(Qh, Q, Fhp, Fhm, area, dx, dt);
   
     % fix to inlet?
     Qh(:,1) = [rho_sp(1); rho_sp(1)*u_sp(1); e_sp(1)] * area(1);
+    
     % extrap for now
     Qh(:,jmax) = Qh(:,jmax-1);
 
@@ -104,6 +69,37 @@ for jmax = jmaxes
     Qh = Qh + res;
   end
 end
+
+function Qh = convert_to_conservative(Qh, rho, u, e, area)
+  % convert to conservative variables
+  Qh(1, :) = rho .* area;
+  Qh(2, :) = rho .* u .* area;
+  Qh(3, :) = e .* area;
+end
+
+function Q = convert_to_primitive(Q, Qh, gamma, area)
+  % convert to primitive variables
+  Q(1, :) = Qh(1, :) ./ area;
+  Q(2, :) = Qh(2, :) ./ Qh(2, :);
+  Q(3, :) = (gamma - 1) .* (Qh(3,:) ./ area - 0.5 .* Q(1,:) .* Q(2,:).^2);
+end
+
+function dt = compute_timestep(cfl, dx, u, c)
+  % compute local timesteps
+  dt_local = cfl * dx ./ (abs(u) + abs(c));
+  dt = min(dt_local, [], "all");
+end
+
+function residual = compute_residual(Qh, Q, Fhp, Fhm, area, dx, dt)
+  % compute residuals
+  N = size(Qh, 2);
+  residual = zeros(3, N);
+  for j = 2:size(Qh, 2)-1
+    residual(:, j) = -dt / dx * (Fhp(:, j) - Fhp(:, j-1) + Fhm(:, j+1) - Fhm(:, j));
+    residual(2, j) = residual(2,j)  + dt / dx * (Q(3,j) * (0.5*(area(j+1)-area(j)) - 0.5*(area(j)-area(j-1))));
+  end  
+end
+
 
 
 % plot results
@@ -115,7 +111,6 @@ e = Qh(3, :) ./ area;
 u = rhou ./ rho;
 u2 = u.^2;
 p = (gamma - 1) .* (e - 0.5 * rho .* u2);
-size(p)
 
 % plot pressure
 figure(2)
